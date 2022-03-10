fd96aa2b0e0f7ab236f9da1709b3d5db48469481
rafael veiga <rafaelveig@hotmail.com>
2022-03-10 17:24:58 -0300

    cria raça dos anoes

---
+++ src/Races/Dwarf.ts
@@ -0,0 +1,20 @@
+import Race from './Race';
+
+export default class Dwarf extends Race {
+  private _maxLifePoints: number;
+  static _createdRacesInstances = 0; // metodo criado no race 
+
+  constructor(name: string, dexterity: number) {
+    super(name, dexterity); // super() chama o construtor da classe pai
+    this._maxLifePoints = 80;
+    Dwarf._createdRacesInstances += 1; // incrementa o numero de instancias criadas da classe Dwarf
+  }
+
+  get maxLifePoints(): number {
+    return this.maxLifePoints;
+  }
+
+  static createdRacesInstances(): number {
+    return Dwarf._createdRacesInstances;
+  }
+}
--- src/Races/index.ts
+++ src/Races/index.ts
@@ -1,3 +1,4 @@
 import Race from './Race';
+import Dwarf from './Dwarf';
 
-export default Race;
+export default { Race, Dwarf };
